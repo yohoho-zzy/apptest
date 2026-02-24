1f7db5652f8a335846c5df4c9f93a84d4dccebd8
yohoho-zzy <1780524624@qq.com>
2026-02-24 15:39:08 +0900

    Fix missing PaddingValues import in ResourceScreen

--- app/src/main/java/com/example/quotepicker/ui/ResourceScreen.kt
+++ app/src/main/java/com/example/quotepicker/ui/ResourceScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
